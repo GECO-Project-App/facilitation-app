8364e75b9af469522b503bf814f425406b033fa0
Alice A <alice.anglesjo@hotmail.com>
2024-09-06 11:46:48 +0200

    fix: resources

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
+  publicExcludes: ['!noprecache/**/*'],
   cacheOnFrontEndNav: true,
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
@@ -14,8 +15,18 @@ const withPWA = withPWAInit({
   },
 });
 
-export default withPWA({
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: 'export',
-});
+};
+
+if (process.env.NEXT_EXPORT === 'true') {
+  nextConfig.output = 'export';
+
+  // Lägg till dessa rader
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '');
+  nextConfig.basePath = `/${repo}`;
+  nextConfig.assetPrefix = `/${repo}/`;
+}
+
+export default withPWA(nextConfig);
--- src/components/LanguageSwitch.tsx
+++ src/components/LanguageSwitch.tsx
@@ -1,15 +1,20 @@
+'use client';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
 
 export const LanguageSwitcher = () => {
+  const router = useRouter();
+  const {pathname, asPath, query} = router;
+
   return (
     <div className="space-x-4">
-      <Link href="/en" locale="en">
+      <Link href={{pathname, query}} as={asPath} locale="en">
         English
       </Link>
-      <Link href="/sv" locale="sv">
+      <Link href={{pathname, query}} as={asPath} locale="sv">
         Swedish
       </Link>
-      <Link href="/nl" locale="nl">
+      <Link href={{pathname, query}} as={asPath} locale="nl">
         Dutch
       </Link>
     </div>
