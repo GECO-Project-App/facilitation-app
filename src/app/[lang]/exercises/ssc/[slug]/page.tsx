95bae83f005c1f9a820d260f7016c5c0f10c499d
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-12 09:24:04 +0200

    Add Tips Components

--- src/app/[lang]/exercises/ssc/[slug]/page.tsx
+++ src/app/[lang]/exercises/ssc/[slug]/page.tsx
@@ -1,8 +1,7 @@
 import {getDictionary} from '../../../dictionaries';
 import {getSsdData} from '@/lib/ssc-mock-data';
-import {Button} from '@/components/ui/button';
 import SSCExercise from '@/components/ssc-exercise';
-
+import Tips from '@/components/ssc-exercise/Tips';
 export async function generateMetadata({params: {lang}}: {params: {lang: string}}) {
   const t = await getDictionary(lang);
   return {
@@ -11,10 +10,13 @@ export async function generateMetadata({params: {lang}}: {params: {lang: string}
   };
 }
 
-export default async function SSC({params}: {params: {lang: string; slug: string; query: string}}) {
+export default async function SSC({params}: {params: {lang: string; slug: string}}) {
   const t = await getDictionary(params.lang);
-  const data = getSsdData(params.slug);
-  console.log(data);
+  const slug = params.slug;
+  const data = getSsdData(slug);
+
+  if (slug === 'tips') return <Tips />
+
   return (
     <main
       className={`page-padding flex min-h-screen flex-col ${params.slug === 'start' ? 'bg-green' : params.slug === 'stop' ? 'bg-orange' : params.slug === 'continue' ? 'bg-pink' : 'bg-blue'}`}>
---
+++ src/components/ssc-exercise/Tips.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Tips: React.FC = () => {
+  return (
+    <div className="tips-container">
+      <h2>Tips for SSC Exercise</h2>
+      <ul>
+        <li>1. Set clear expectations</li>
+        <li>2. Provide a visual aid</li>
+        <li>3. Stay on track</li>
+        <li>4. Record responses</li>
+        <li>5. Analyze feedback and create a plan of action</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Tips;
