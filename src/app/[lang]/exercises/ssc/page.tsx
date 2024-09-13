ee8b1776cfdb438479d41943978a134a716db94d
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-13 11:00:15 +0200

    Fix back key

--- src/app/[lang]/exercises/ssc/page.tsx
+++ src/app/[lang]/exercises/ssc/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import {getDictionary} from '../../dictionaries';
 import {Light} from '@/components/Light/Light';
 import './ssc.css';
+import {ArrowLeft} from 'lucide-react';
+
 export async function generateMetadata({params: {lang}}: {params: {lang: string}}) {
   const t = await getDictionary(lang);
   return {
@@ -16,11 +18,15 @@ export default async function Checkin({params}: {params: {lang: string}}) {
 
   return (
     <main className="page-padding flex min-h-screen flex-col bg-blue">
-      <NavBar />
-      <section className="flex flex-1 flex-col items-center justify-evenly">
-         <Link href={'/exercises/ssc/tips'} className='self-end pr-[10%]'>
+      <section className='flex flex-row justify-between'>
+      <Link href={'/'}>
+        <ArrowLeft size={60} />
+      </Link>
+        <Link href={'/exercises/ssc/tips'} className="self-end pr-[10%]">
           <Light />
         </Link>
+      </section>
+      <section className="flex flex-1 flex-col items-center justify-evenly">
         <Link href={'/exercises/ssc/introduction?step=start'}>
           <Button variant="checkout" hasShadow className="mx-auto" size="circle">
             START
@@ -40,7 +46,7 @@ export default async function Checkin({params}: {params: {lang: string}}) {
           <Button variant="back" hasShadow className="mx-auto">
             Feedback
           </Button>
-        </Link> 
+        </Link>
       </section>
     </main>
   );
