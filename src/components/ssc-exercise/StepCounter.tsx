e27c48e73953ae56abd916b7c0d0ab9e5b0f40e8
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-23 13:27:03 +0200

    ad bg color to stepper

--- src/components/ssc-exercise/StepCounter.tsx
+++ src/components/ssc-exercise/StepCounter.tsx
@@ -12,7 +12,13 @@ const StepCounter: React.FC<StepCounterProps> = ({ currentStep, length }) => {
   return (
     <section className="flex items-center justify-center text-center">
       <div className="text-5xl pb-5">{dotsBefore}</div>
-      <h2 className="text-4xl font-semibold border-2 border-black rounded-full p-1 w-12 h-12">{currentStep}</h2>
+      <h2 className={`text-4xl font-semibold border-2 border-black rounded-full p-1 w-12 h-12 ${
+        currentStep === 1 ? 'bg-orange' :
+        currentStep === 2 ? 'bg-orange' :
+        currentStep === 3 ? 'bg-pink' :
+        currentStep === 4 ? 'bg-blue text-white' :
+        currentStep === 5 ? 'bg-blue text-white' : ''    
+    }`}>{currentStep}</h2>
       <div className="text-5xl pb-5">{dotsAfter}</div>
     </section>
   );
