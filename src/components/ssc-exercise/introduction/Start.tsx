6522d29c7bf3197227183a90e69dc4c050262f09
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-23 13:00:01 +0200

    add padding to introduction pages at ssc

--- src/components/ssc-exercise/introduction/Start.tsx
+++ src/components/ssc-exercise/introduction/Start.tsx
@@ -5,13 +5,17 @@ const Start: React.FC = () => {
     <section>
       <header>
         {/* <img src="/assets/svg/start-intro.svg" alt="Start" /> */}
-        <RiveAnimation src="/assets/riv/ssc_startgecko.riv" width="160px" height="160px" />
-        <h1 className="text-2xl font-bold">
+        <div className="text-center">
+          <div className="flex justify-center">
+            <RiveAnimation src="/assets/riv/ssc_startgecko.riv" />
+          </div>
+        </div>
+        <h1 className="text-2xl font-bold pt-2">
           Chapter 1:<span className="text-amber-300">Start</span>
         </h1>
         <span className="text-sm text-gray-500">4-5 minutes | 2-20 members</span>
       </header>
-      <p>
+      <p className="pt-4">
         In this chapter, you will brainstorm and discuss what are the things you could{' '}
         <span className="font-bold">start</span> doing as a community to better support the
         productivity, communication, etc. Give an example of how this might help you and the
