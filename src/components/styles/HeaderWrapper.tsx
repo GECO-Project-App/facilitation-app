dfd4770f2163ca41f92f91d6afa34b652aaff4b4
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-23 13:03:36 +0200

    deleted extra bg

--- src/components/styles/HeaderWrapper.tsx
+++ src/components/styles/HeaderWrapper.tsx
@@ -17,7 +17,7 @@ const HeaderWrapper: FC<HeaderWrapperProps> = ({title, handleBack }) => {
     <h1 className="rounded-full border-2 border-black bg-yellow pr-4 pl-4 pt-1 pb-1 text-xl font-bold">
       {title}
     </h1>
-    <div className="h-10 w-10 rounded-full bg-yellow"></div>
+    <div className="h-10 w-10 rounded-full"></div>
   </header>
 )
 
