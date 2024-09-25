5a0ab60c172c28702192bb705a453ec83b50ecd4
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-25 14:38:03 +0200

    add survey from Posthog

--- src/app/[lang]/exercises/feedback/[slug]/page.tsx
+++ src/app/[lang]/exercises/feedback/[slug]/page.tsx
@@ -1,30 +1,31 @@
 'use client';
 import React, {useState, useEffect} from 'react';
 import Survey from './Survey';
-import { usePostHog } from 'posthog-js/react';
+import {usePostHog} from 'posthog-js/react';
 
 export default function FeedbackFor({params}: {params: {lang: string; slug: string}}) {
   const slug = params.slug;
   const [showSurvey, setShowSurvey] = useState(true);
 
-  const posthog = usePostHog()
+  const posthog = usePostHog();
+
   useEffect(() => {
     posthog.getActiveMatchingSurveys((surveys) => {
       console.log(surveys);
-    }); 
+    });
   }, [posthog]);
+
   const handleSubmit = (value: string | null) => {
     console.log(value);
   };
-  
 
   return (
-    <main className="page-padding flex min-h-screen flex-col items-center justify-evenly bg-yellow">
-      {showSurvey && (  
+    <main className="bg-yellow min-h-screen">
+      {showSurvey && (
         <Survey
-          title={slug === 'ssc' ? 'Start-Stop-Continue Exercise' : ''}
-        onSubmit={handleSubmit}
-      />
+          title={slug === 'ssc' ? 'Start-Stop-Continue' : ''}
+          onSubmit={handleSubmit}
+        />
       )}
     </main>
   );
