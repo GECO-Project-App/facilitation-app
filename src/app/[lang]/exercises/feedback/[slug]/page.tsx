c9969d4bff1262d55d8742d6bd9e9cf99c0f3129
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-25 13:50:37 +0200

    Extra lines deleted

--- src/app/[lang]/exercises/feedback/[slug]/page.tsx
+++ src/app/[lang]/exercises/feedback/[slug]/page.tsx
@@ -1,8 +1,5 @@
-'use client'
+'use client';
 import React from 'react';
-import Link from 'next/link';
-import {ArrowLeft} from 'lucide-react';
-import {Button} from '@/components/ui/button';
 import Survey from './Survey';
 
 export default function FeedbackFor({params}: {params: {lang: string; slug: string}}) {
@@ -14,10 +11,10 @@ export default function FeedbackFor({params}: {params: {lang: string; slug: stri
 
   return (
     <main className="page-padding flex min-h-screen flex-col items-center justify-evenly bg-yellow">
-    {/* <h4 className="text-lg font-semibold">
-      Have some thoughts, comments and suggestions about the app? Share your thought with us.
-    </h4> */}
-    <Survey title={slug === 'ssc' ? 'Start-Stop-Continue Exercise' : ''} onSubmit={handleSubmit} />
-  </main>
-  )
+      <Survey
+        title={slug === 'ssc' ? 'Start-Stop-Continue Exercise' : ''}
+        onSubmit={handleSubmit}
+      />
+    </main>
+  );
 }
