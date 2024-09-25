7209f39eb62d8742997ca3b932f7017f33ae93bb
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-25 13:45:59 +0200

    impelmented text area

--- src/app/[lang]/exercises/feedback/[slug]/Survey.tsx
+++ src/app/[lang]/exercises/feedback/[slug]/Survey.tsx
@@ -1,31 +1,45 @@
-'use client'
-
-import { useState } from 'react'
+'use client';
+import {useState} from 'react';
+import Link from 'next/link';
+import {ArrowLeft} from 'lucide-react';
+import {Button} from '@/components/ui/button';
 
 interface SurveyProps {
   title: string;
-  onSubmit: (value: number | null) => void;
+  onSubmit: (value: string | null) => void;
 }
 
-function Survey({ title, onSubmit }: SurveyProps) {
-  const [selectedValue, setSelectedValue] = useState<number | null>(null);
-
-  const handleSelect = (value: number) => {
-    setSelectedValue(value);
-  }
+function Survey({title, onSubmit}: SurveyProps) {
+  const [selectedValue, setSelectedValue] = useState('');
 
   const handleSubmit = () => {
     onSubmit(selectedValue);
-  }
-  
+  };
+
   return (
-    <article>
-      <h2>{title}</h2>
-      <textarea></textarea>
-        <button onClick={handleSubmit}>Submit</button>
-      {/* <div>
-        <button className={styles.button} onClick={handleSubmit}>Submit</button>
-      </div> */}
+    <article className="page-padding flex min-h-screen flex-col justify-evenly">
+        <section className="flex w-full flex-row items-center justify-evenly gap-4">
+          <Link href={'/exercises/ssc'}>
+            <ArrowLeft size={42} />
+          </Link>
+          <header className="text-xl font-bold">
+            <h2>{title}</h2>
+            <h2>Exercise</h2>
+          </header>
+        </section>
+        <div>
+          <textarea
+            className="h-40 w-full rounded-lg border-2 border-black bg-amber-50 p-4"
+            placeholder="Have some thoughts, comments and suggestions about the app? Share your thought with us."
+            value={selectedValue}
+            onChange={(e) => setSelectedValue(e.target.value)}
+          />
+        </div>
+        <section className="flex justify-center">
+          <Button variant="yellow" onClick={handleSubmit}>
+            Send Us Feedback
+          </Button>
+        </section>
     </article>
   );
 }
--- src/app/[lang]/exercises/feedback/[slug]/page.tsx
+++ src/app/[lang]/exercises/feedback/[slug]/page.tsx
@@ -8,7 +8,7 @@ import Survey from './Survey';
 export default function FeedbackFor({params}: {params: {lang: string; slug: string}}) {
   const slug = params.slug;
 
-  const handleSubmit = (value: number | null) => {
+  const handleSubmit = (value: string | null) => {
     // setShowSurvey(false);
     console.log('Test');
   };
@@ -16,22 +16,10 @@ export default function FeedbackFor({params}: {params: {lang: string; slug: stri
 
   return (
     <main className="page-padding flex min-h-screen flex-col items-center justify-evenly bg-yellow">
-    <section className="flex w-full flex-row items-center justify-evenly gap-4">
-      <Link href={'/exercises/ssc'}>
-        <ArrowLeft size={42} />
-      </Link>
-      <div className="text-xl font-bold">
-        {slug === 'ssc' ? <h2>Start-Stop-Continue</h2> : null}
-        <h2>Exercise</h2>
-      </div>
-    </section>
-    <h4 className="text-lg font-semibold">
+    {/* <h4 className="text-lg font-semibold">
       Have some thoughts, comments and suggestions about the app? Share your thought with us.
-    </h4>
-    <Survey title="Feedback" onSubmit={handleSubmit} />
-        <section className="flex justify-center">
-      <Button variant="yellow">Send Us Feedback</Button>
-    </section>
+    </h4> */}
+    <Survey title={slug === 'ssc' ? 'Start-Stop-Continue Exercise' : ''} onSubmit={handleSubmit} />
   </main>
   )
 }
