42a6373e7e653d31792d0914f70994ac7a1ad994
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-26 12:15:56 +0200

    fix text area

--- src/app/[lang]/exercises/feedback/[slug]/Survey.tsx
+++ src/app/[lang]/exercises/feedback/[slug]/Survey.tsx
@@ -25,10 +25,10 @@ function Survey({title, onSubmit}: SurveyProps) {
   };
 
   return (
-    <article className="page-padding flex min-h-screen flex-col justify-evenly">
+    <article className="page-padding flex min-h-screen flex-col justify-between">
       <HeaderWrapper title="Feedback" handleBack={handleBack} />
       <div>
-        <div className="relative">
+        <div className="relative h-[60vh]">
           <div className="absolute left-0 top-0 w-full rounded-t-3xl border-l-2 border-r-2 border-t-2 border-black bg-amber-50 bg-pink p-2 text-lg font-bold text-black">
             {title}
             <div className="text-xs font-normal text-gray-700">
@@ -37,8 +37,9 @@ function Survey({title, onSubmit}: SurveyProps) {
             <div className="text-xs font-normal text-gray-700">What didn't?</div>
           </div>
           <textarea
-            className="h-80 w-full rounded-3xl border-2 border-black p-1 pt-20 shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
+            className="h-full w-full rounded-3xl border-2 border-black p-1 pt-20 shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
             value={selectedValue}
+            placeholder="Write your feedback here..."
             onChange={(e) => setSelectedValue(e.target.value)}
           />
         </div>
