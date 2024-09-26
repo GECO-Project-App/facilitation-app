fc9533519127133d17b09353e7768ea8e894884c
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-26 23:40:05 +0200

    Put all sarvey IDS

--- src/app/[lang]/exercises/feedback/[slug]/page.tsx
+++ src/app/[lang]/exercises/feedback/[slug]/page.tsx
@@ -2,16 +2,39 @@
 import React, {useState, useEffect} from 'react';
 import Survey from './Survey';
 import {usePostHog} from 'posthog-js/react';
-
+import { SSC_SURVEY_ID, CH_IN_SURVEY_ID, CH_OUT_SURVEY_ID } from '@/lib/surveys-id';
 export default function FeedbackFor({params}: {params: {lang: string; slug: string}}) {
   const slug = params.slug;
   const [showSurvey, setShowSurvey] = useState(true);
 
+  const [surveyTitle, setSurveyTitle] = useState('');
+  const [surveyID, setSurveyID] = useState('');
+
   const posthog = usePostHog();
+
+  const getCurrentSurvey = (slug: string) => {
+    switch (slug) {
+      case 'ssc':
+        return SSC_SURVEY_ID;
+      case 'check-in':
+        return CH_IN_SURVEY_ID;
+      case 'check-out':
+        return CH_OUT_SURVEY_ID;
+      default:
+        return '';
+    }
+  }
 
   useEffect(() => {
     posthog.getActiveMatchingSurveys((surveys) => {
       console.log(surveys);
+      if (surveys.length > 0) {
+        const survey = surveys.find(survey => survey.id === getCurrentSurvey(slug));
+        if (survey) {
+          setSurveyID(survey.id);
+          setSurveyTitle(survey.questions[0].question)
+        }
+      }
     });
   }, [posthog]);
 
@@ -19,11 +42,13 @@ export default function FeedbackFor({params}: {params: {lang: string; slug: stri
     console.log(value);
   };
 
+  console.log(surveyID, surveyTitle);
+
   return (
     <main className="bg-yellow min-h-screen">
       {showSurvey && (
         <Survey
-          title={slug === 'ssc' ? 'Start-Stop-Continue' : ''}
+          title={slug === 'ssc' ? 'Start-Stop-Continue' : slug === 'check-in' ? 'Check-In' : slug === 'check-out' ? 'Check-Out' : ''}
           onSubmit={handleSubmit}
         />
       )}
---
+++ src/lib/surveys-id.ts
@@ -0,0 +1,4 @@
+export const SSC_SURVEY_ID = "01922903-7dcd-0000-896b-10b87d7e1398";
+export const CH_IN_SURVEY_ID = "0192300f-bfbc-0000-eb00-eadb26e2840b";
+export const CH_OUT_SURVEY_ID = "01923010-6ebe-0000-b046-acb9f1912507";
+
