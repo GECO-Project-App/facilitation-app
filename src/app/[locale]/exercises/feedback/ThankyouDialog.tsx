1e0862531e3a2f5535cbdcea25a52f3b22f96980
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-27 14:58:43 +0200

    Add Icon

--- src/app/[locale]/exercises/feedback/ThankyouDialog.tsx
+++ src/app/[locale]/exercises/feedback/ThankyouDialog.tsx
@@ -6,17 +6,19 @@ import {
     DialogTitle,
     DialogTrigger,
   } from "@/components/ui/dialog"
-
+import { Confetti } from "@/components/icons/confetti"
 export default function ThankyouDialog() {
   return (
     <Dialog>
       <DialogTrigger>Open</DialogTrigger>
-      <DialogContent>
+      <DialogContent className="bg-pink">
     <DialogHeader>
-      <DialogTitle>Are you absolutely sure?</DialogTitle>
-      <DialogDescription>
-        This action cannot be undone. This will permanently delete your account
-        and remove your data from our servers.
+      <DialogTitle className="text-2xl font-bold text-black">Thank you for your feedback!</DialogTitle>
+      <DialogDescription className="font-family-j">
+        <Confetti />
+        <h2>
+            Thank you for your feedback!
+            </h2>
       </DialogDescription>
     </DialogHeader>
       </DialogContent>
