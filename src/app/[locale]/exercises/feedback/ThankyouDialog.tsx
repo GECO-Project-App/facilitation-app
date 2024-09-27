941f88efc3ac48d003be96091ac5c7bfbeb77a0c
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-27 16:09:26 +0200

    dialog defaultOpen

--- src/app/[locale]/exercises/feedback/ThankyouDialog.tsx
+++ src/app/[locale]/exercises/feedback/ThankyouDialog.tsx
@@ -1,16 +1,16 @@
+import React from 'react';
 import {
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
 import {Confetti} from '@/components/icons/confetti';
+
 export default function ThankyouDialog() {
   return (
-    <Dialog>
-      <DialogTrigger>Open</DialogTrigger>
+    <Dialog defaultOpen={true}>
       <DialogContent className="bg-pink">
         <DialogHeader className="flex flex-col items-center justify-center h-full">
           <DialogTitle className="text-2xl font-bold text-black flex flex-col items-center gap-2">
