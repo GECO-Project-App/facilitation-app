6ebdf0827f0ea7f7adaf378b567954b43509552a
Cristian Nass <cristian.tirdad@gmail.com>
2024-09-27 15:57:23 +0200

    dialog is centred

--- src/app/[locale]/exercises/feedback/ThankyouDialog.tsx
+++ src/app/[locale]/exercises/feedback/ThankyouDialog.tsx
@@ -1,26 +1,26 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { Confetti } from "@/components/icons/confetti"
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import {Confetti} from '@/components/icons/confetti';
 export default function ThankyouDialog() {
   return (
     <Dialog>
       <DialogTrigger>Open</DialogTrigger>
       <DialogContent className="bg-pink">
-    <DialogHeader>
-      <DialogTitle className="text-2xl font-bold text-black">Thank you for your feedback!</DialogTitle>
-      <DialogDescription className="font-family-j">
-        <Confetti />
-        <h2>
-            Thank you for your feedback!
-            </h2>
-      </DialogDescription>
-    </DialogHeader>
+        <DialogHeader className="flex flex-col items-center justify-center h-full">
+          <DialogTitle className="text-2xl font-bold text-black flex flex-col items-center gap-2">
+            <>
+              <Confetti />
+              Thank you for your feedback!
+            </>
+          </DialogTitle>
+          <DialogDescription className="font-family-j"></DialogDescription>
+        </DialogHeader>
       </DialogContent>
     </Dialog>
   );
