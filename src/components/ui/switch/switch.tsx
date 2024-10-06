6772554cbfa48ca12a24131c2d0f7fda53fae3ac
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-06 14:51:51 +0200

    fix sizes

--- src/components/ui/switch/switch.tsx
+++ src/components/ui/switch/switch.tsx
@@ -8,7 +8,8 @@ import {cn} from '@/lib/utils';
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
->(({className, ...props}, ref) => (
+>(({className, ...props}, ref) => {
+  return (
   <SwitchPrimitives.Root
     className={cn(
       'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-4 border-black transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-backgrounddisabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green data-[state=unchecked]:bg-sky-300',
@@ -19,12 +20,16 @@ const Switch = React.forwardRef<
       
     <SwitchPrimitives.Thumb
       className={cn(
-        'pointer-events-none block h-20 w-40 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-40 data-[state=unchecked]:translate-x-[-3px] data-[state=checked]:bg-sky-300 data-[state=unchecked]:bg-green  border-4 border-black',
+        'pointer-events-none block h-20 w-40 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-32 data-[state=unchecked]:translate-x-[-3px] data-[state=checked]:bg-sky-300 data-[state=unchecked]:bg-green  border-4 border-black',
       )}
       />
-    <div className="text-black text-2xl font-bold ml-6">Signup</div>
+    <div className="text-black text-2xl font-bold ml-4">Signup</div>
   </SwitchPrimitives.Root>
-));
+);
+});
+
+
+
 Switch.displayName = SwitchPrimitives.Root.displayName;
 
 export {Switch};
