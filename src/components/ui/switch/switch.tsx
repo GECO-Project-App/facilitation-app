9a1b85850dd37a8d2d65ac5f5cebbe72e8789f94
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-06 14:19:49 +0200

    add text

--- src/components/ui/switch/switch.tsx
+++ src/components/ui/switch/switch.tsx
@@ -1,29 +1,29 @@
-"use client"
+'use client';
 
-import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
+import * as React from 'react';
+import * as SwitchPrimitives from '@radix-ui/react-switch';
 
-import { cn } from "@/lib/utils"
+import {cn} from '@/lib/utils';
 
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
->(({ className, ...props }, ref) => (
+>(({className, ...props}, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green data-[state=unchecked]:bg-blue",
-      className
+      'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-4 border-black transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-backgrounddisabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green data-[state=unchecked]:bg-blue bo',
+      className,
     )}
     {...props}
-    ref={ref}
-  >
+    ref={ref}>
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-20 w-40 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-40 data-[state=checked]:bg-blue data-[state=unchecked]:translate-x-0 data-[state=unchecked]:bg-green"
+        'pointer-events-none block h-20 w-40 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-40 data-[state=unchecked]:translate-x-0 data-[state=checked]:bg-blue data-[state=unchecked]:bg-green  border-4 border-black',
       )}
     />
+    <div className="text-white text-2xl font-bold ml-8">Login</div>
   </SwitchPrimitives.Root>
-))
-Switch.displayName = SwitchPrimitives.Root.displayName
+));
+Switch.displayName = SwitchPrimitives.Root.displayName;
 
-export { Switch }
+export {Switch};
