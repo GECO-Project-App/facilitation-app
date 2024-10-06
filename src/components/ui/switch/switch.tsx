d76338c11431872de9a99965a96de732926292e9
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-06 14:34:14 +0200

    change text

--- src/components/ui/switch/switch.tsx
+++ src/components/ui/switch/switch.tsx
@@ -11,17 +11,18 @@ const Switch = React.forwardRef<
 >(({className, ...props}, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-4 border-black transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-backgrounddisabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green data-[state=unchecked]:bg-blue bo',
+      'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-4 border-black transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-backgrounddisabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green data-[state=unchecked]:bg-sky-300',
       className,
     )}
     {...props}
     ref={ref}>
+      
     <SwitchPrimitives.Thumb
       className={cn(
-        'pointer-events-none block h-20 w-40 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-40 data-[state=unchecked]:translate-x-0 data-[state=checked]:bg-blue data-[state=unchecked]:bg-green  border-4 border-black',
+        'pointer-events-none block h-20 w-40 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-40 data-[state=unchecked]:translate-x-[-3px] data-[state=checked]:bg-sky-300 data-[state=unchecked]:bg-green  border-4 border-black',
       )}
-    />
-    <div className="text-white text-2xl font-bold ml-8">Login</div>
+      />
+    <div className="text-black text-2xl font-bold ml-6">Signup</div>
   </SwitchPrimitives.Root>
 ));
 Switch.displayName = SwitchPrimitives.Root.displayName;
