f64b27c9932c64d6b34376e3406756abf3c8827d
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-08 15:48:47 +0200

    implementerd ssc v2

--- src/components/ssc-exercise/index.tsx
+++ src/components/ssc-exercise/index.tsx
@@ -8,10 +8,21 @@ import {sscMock} from '@/lib/mock';
 import {Step} from '@/lib/types';
 import {useRouter} from '@/navigation';
 import {ArrowRight} from 'lucide-react';
-import React, {useMemo, useState} from 'react';
+import React, {useMemo, useState, useEffect} from 'react';
 import FooterWrapper from '@/components/styles/FooterWrapper';
 import {Header} from '@/components';
 import {CarouselPagination} from '@/components/CarouselPagination';
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+import {Link} from '@/navigation';
+import {Complete} from '@/components/icons';
+import {useTranslations} from 'next-intl';
 
 export type SSCExerciseProps = {
   chapter: string;
@@ -21,6 +32,8 @@ export type SSCExerciseProps = {
 const SSCExercise: React.FC<SSCExerciseProps> = ({chapter, steps}) => {
   const router = useRouter();
   const [currentStep, setCurrentStep] = useState(0);
+  const t = useTranslations('exercises.ssc');
+  const [api, setApi] = useState<CarouselApi>();
 
   const chapterMap = {
     start: sscMock.start.steps,
@@ -66,45 +79,62 @@ const SSCExercise: React.FC<SSCExerciseProps> = ({chapter, steps}) => {
   }
 
   const currentStepData = steps[currentStep];
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
 
+    setCurrentStep(api.selectedScrollSnap());
+
+    api.on('select', () => {
+      setCurrentStep(api.selectedScrollSnap());
+    });
+  }, [api]);
   return (
     <PageLayout
-      backgroundColor={sscMock[chapter as keyof typeof chapterMap].backgroundColor}
+      backgroundColor="bg-blue"
       header={
         <Header>
           <CarouselPagination steps={steps} currentStep={currentStep} />
         </Header>
+      }
+      footer={
+        currentStep === steps.length - 1 ? (
+          <Button variant="blue" className="mx-auto" asChild>
+            <Link href={`/ssc/accomplishment`}>
+              {/* {t('completeButton')} <Complete stroke="white" /> */}
+              Complete Button
+              <Complete stroke="white" />
+            </Link>
+          </Button>
+        ) : (
+          <Button variant="yellow" className="mx-auto" asChild>
+            {/* <Link href="/">{t('homeButton')}</Link> */}
+            <Link href="/">Home</Link>
+          </Button>
+        )
       }>
-      <article className="flex h-40 flex-col items-center justify-between">
-        {/* <HeaderWrapper
-          title={currentStepData.title}
-          handleBack={handlePreviousStep}
-          currentStep={currentStep}
-        /> */}
-        {/* <StepCounter currentStep={currentStep} length={steps.length - 1} /> */}
-      </article>
-      <article className="flex flex-1 flex-col items-center justify-evenly">
-        {chapterSteps[currentStep]?.sticker && (
-          <RiveAnimation
-            key={chapterSteps[currentStep].sticker}
-            src={chapterSteps[currentStep].sticker}
-            width={300}
-          />
-        )}
-        <DescriptionWrapper>{currentStepData.description}</DescriptionWrapper>
-        {chapterSteps[currentStep]?.timer && <Timer seconds={chapterSteps[currentStep].timer} />}
-        {/* <footer className="mt-8">
-          <Button variant="pink" onClick={handleNextStep}>
-            {currentStepData.button} <ArrowRight size={28} />
-          </Button>
-        </footer>  */}
-        <FooterWrapper
-          backgroundColor={sscMock[chapter as keyof typeof chapterMap].backgroundColor}>
-          <Button variant="pink" onClick={handleNextStep}>
-            {currentStepData.button} <ArrowRight size={28} />
-          </Button>
-        </FooterWrapper>
-      </article>
+      <section className="flex h-full w-full flex-1 items-center justify-center">
+        <Carousel className="h-full w-full flex-1" setApi={setApi}>
+          <CarouselContent>
+            {steps.map((_, index) => (
+              <CarouselItem key={index} className="space-y-6">
+                <p className="text-2xl">{steps[index].description}</p>
+                <div className="relative aspect-video">
+                  {chapterSteps[index].sticker && (
+                    <RiveAnimation src={chapterSteps[index].sticker} width="100%" height="100%" />
+                  )}
+                  {chapterSteps[index].timer && <Timer seconds={chapterSteps[index].timer} />}
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="flex flex-row items-center justify-between">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        </Carousel>
+      </section>
     </PageLayout>
   );
 };
