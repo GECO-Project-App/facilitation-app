3f27a591934b44b8a2546e7fafbf79f3dbf7c6e4
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-08 19:54:33 +0200

    add hamburger manu

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -7,11 +7,31 @@ const AppBar: React.FC = () => {
   return (
     <div className="sticky top-0 flex h-[40px] w-full items-center justify-between border-b-2 border-black bg-yellow p-4">
       <div>
+        <Link href={'/log-in'}>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round">
+              <line x1="3" y1="12" x2="21" y2="12" />
+              <line x1="3" y1="6" x2="21" y2="6" />
+              <line x1="3" y1="18" x2="21" y2="18" />
+            </svg>
+          </div>
+        </Link>
+      </div>
+      <div>
         <h1 className="text-2xl font-bold text-white"></h1>
       </div>
       <div>
-        <Link href={'/log-in'}>
-            <Setting />
+        <Link href={'#'}>
+          <Setting />
         </Link>
       </div>
     </div>
