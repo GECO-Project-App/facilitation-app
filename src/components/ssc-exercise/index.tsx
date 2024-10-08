fb11b56c8f7fc1467486f57b26e8f2f523c8cd7f
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-08 16:12:39 +0200

    commin tome line for future

--- src/components/ssc-exercise/index.tsx
+++ src/components/ssc-exercise/index.tsx
@@ -1,15 +1,9 @@
 'use client';
 import {PageLayout, RiveAnimation, Timer} from '@/components';
-import StepCounter from '@/components/ssc-exercise/StepCounter';
-import DescriptionWrapper from '@/components/styles/DescriptionWrapper';
-import HeaderWrapper from '@/components/styles/HeaderWrapper';
 import {Button} from '@/components/ui/button';
 import {sscMock} from '@/lib/mock';
 import {Step} from '@/lib/types';
-import {useRouter} from '@/navigation';
-import {ArrowRight} from 'lucide-react';
 import React, {useMemo, useState, useEffect} from 'react';
-import FooterWrapper from '@/components/styles/FooterWrapper';
 import {Header} from '@/components';
 import {CarouselPagination} from '@/components/CarouselPagination';
 import {
@@ -22,7 +16,6 @@ import {
 } from '@/components/ui/carousel';
 import {Link} from '@/navigation';
 import {Complete} from '@/components/icons';
-import {useTranslations} from 'next-intl';
 
 export type SSCExerciseProps = {
   chapter: string;
@@ -30,9 +23,9 @@ export type SSCExerciseProps = {
 };
 
 const SSCExercise: React.FC<SSCExerciseProps> = ({chapter, steps}) => {
-  const router = useRouter();
+  // const router = useRouter();
   const [currentStep, setCurrentStep] = useState(0);
-  const t = useTranslations('exercises.ssc');
+  // const t = useTranslations('exercises.ssc');
   const [api, setApi] = useState<CarouselApi>();
 
   const chapterMap = {
@@ -50,35 +43,35 @@ const SSCExercise: React.FC<SSCExerciseProps> = ({chapter, steps}) => {
     return requiredChapters.every((chapter) => completedChapters.includes(chapter));
   };
 
-  const handleNextStep = () => {
-    if (currentStep === steps.length - 1) {
-      const completedChapters = JSON.parse(localStorage.getItem('chapterDone') || '[]');
-      if (!completedChapters.includes(chapter)) {
-        completedChapters.push(chapter);
-        localStorage.setItem('chapterDone', JSON.stringify(completedChapters));
-      }
+  // const handleNextStep = () => {
+  //   if (currentStep === steps.length - 1) {
+  //     const completedChapters = JSON.parse(localStorage.getItem('chapterDone') || '[]');
+  //     if (!completedChapters.includes(chapter)) {
+  //       completedChapters.push(chapter);
+  //       localStorage.setItem('chapterDone', JSON.stringify(completedChapters));
+  //     }
 
-      router.push(
-        areAllChaptersComplete(completedChapters) ? '/ssc/feedback' : '/ssc/accomplishment',
-      );
-    } else {
-      setCurrentStep((prev) => prev + 1);
-    }
-  };
+  //     router.push(
+  //       areAllChaptersComplete(completedChapters) ? '/ssc/feedback' : '/ssc/accomplishment',
+  //     );
+  //   } else {
+  //     setCurrentStep((prev) => prev + 1);
+  //   }
+  // };
 
-  const handlePreviousStep = () => {
-    if (currentStep === 0) {
-      router.back();
-    } else {
-      setCurrentStep((prev) => prev - 1);
-    }
-  };
+  // const handlePreviousStep = () => {
+  //   if (currentStep === 0) {
+  //     router.back();
+  //   } else {
+  //     setCurrentStep((prev) => prev - 1);
+  //   }
+  // };
 
   if (!steps) {
     return <div>Loading...</div>;
   }
 
-  const currentStepData = steps[currentStep];
+  // const currentStepData = steps[currentStep];
   useEffect(() => {
     if (!api) {
       return;
@@ -110,7 +103,7 @@ const SSCExercise: React.FC<SSCExerciseProps> = ({chapter, steps}) => {
         ) : (
           <Button variant="yellow" className="mx-auto" asChild>
             {/* <Link href="/">{t('homeButton')}</Link> */}
-            <Link href="/">Home</Link>
+            <Link href="/">Back to menu</Link>
           </Button>
         )
       }>
