56c8cacbecfacd411407bc8cf30b56237551dd6b
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-09 16:25:13 +0200

    add Carousel to user page

--- src/app/[locale]/user/page.tsx
+++ src/app/[locale]/user/page.tsx
@@ -1,52 +1,59 @@
 'use client';
+import {useState} from 'react';
 import SignUp from './SignUp';
 import LogIn from './LogIn';
-import {motion} from 'framer-motion';
 import {Tabs, TabsList, TabsTrigger, TabsContent} from '@/components/ui/tabs/tabs';
 import {useUserStore} from '@/store/userStore';
 import ProfilePage from '../profile/page';
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 const AuthPage = () => {
   const user = useUserStore((state) => state.user);
+  const [api, setApi] = useState<CarouselApi>();
+
   return (
     <>
       {user ? (
-        <ProfilePage/>
+        <ProfilePage />
       ) : (
-        <Tabs defaultValue="login" className="overflow-hidden p-4">
-          <TabsList className="m-auto mt-14 grid h-12 w-80 grid-cols-2 bg-sky-300">
-            <TabsTrigger
-              value="login"
-              className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black">
-              Login
-            </TabsTrigger>
-            <TabsTrigger
-              value="signup"
-              className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black">
-              Signup
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="login" className="mt-32">
-            <motion.div
-              key="login"
-              initial={{x: -300, opacity: 0}}
-              animate={{x: 0, opacity: 1}}
-              exit={{x: 300, opacity: 0}}
-              transition={{duration: 0.6}}>
-              <LogIn />
-            </motion.div>
-          </TabsContent>
-          <TabsContent value="signup" className="mt-32">
-            <motion.div
-              key="signup"
-              initial={{x: 300, opacity: 0}}
-              animate={{x: 0, opacity: 1}}
-              exit={{x: -300, opacity: 0}}
-              transition={{duration: 0.6}}>
-              <SignUp />
-            </motion.div>
-          </TabsContent>
-        </Tabs>
+        // <Tabs defaultValue="login" className="overflow-hidden p-4">
+        //   <TabsList className="m-auto mt-14 grid h-12 w-80 grid-cols-2 bg-sky-300">
+        //     <TabsTrigger
+        //       value="login"
+        //       className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black">
+        //       Login
+        //     </TabsTrigger>
+        //     <TabsTrigger
+        //       value="signup"
+        //       className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black">
+        //       Signup
+        //     </TabsTrigger>
+        //   </TabsList>
+        //   <TabsContent value="login" className="mt-32">
+        //     <LogIn />
+        //   </TabsContent>
+        //   <TabsContent value="signup" className="mt-32">
+        //     <SignUp />
+        //   </TabsContent>
+        <>
+          <Carousel className="h-full w-full flex-1" setApi={setApi}>
+          <CarouselContent>
+              <CarouselItem className="space-y-6">
+                <LogIn />
+              </CarouselItem>
+              <CarouselItem className="space-y-6">
+                <SignUp />
+              </CarouselItem>
+            </CarouselContent>
+        </Carousel>
+        </>
       )}
     </>
   );
