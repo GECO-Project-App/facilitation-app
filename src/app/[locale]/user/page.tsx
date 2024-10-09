a97f5d08ad2059aca3867100cac28db80e44af76
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-09 16:49:54 +0200

    fix tabs button

--- src/app/[locale]/user/page.tsx
+++ src/app/[locale]/user/page.tsx
@@ -18,31 +18,40 @@ const AuthPage = () => {
   const user = useUserStore((state) => state.user);
   const [api, setApi] = useState<CarouselApi>();
 
+  const loginTab = () => {
+    api?.scrollPrev();
+  };
+
+  const signupTab = () => {
+    api?.scrollNext();
+  };
+
   return (
     <>
       {user ? (
         <ProfilePage />
       ) : (
-        <Tabs defaultValue="login" className="overflow-hidden p-4">
+        <Tabs defaultValue="login" className="overflow-hidden p-4 h-full">
           <TabsList className="m-auto mt-14 grid h-12 w-80 grid-cols-2 bg-sky-300">
             <TabsTrigger
               value="login"
-              className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black">
+              className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black"
+              onClick={loginTab}>
               Login
             </TabsTrigger>
             <TabsTrigger
               value="signup"
-              className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black">
+              className="text-green-100 h-10 border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black"
+              onClick={signupTab}>
               Signup
             </TabsTrigger>
           </TabsList>
-          <Carousel className="h-full w-full flex-1" setApi={setApi}>
+          <Carousel setApi={setApi}>
             <CarouselContent>
-              <CarouselItem className="space-y-6">
-                <LogIn />
+              <CarouselItem className="space-y-6" key="login">
+                  <LogIn />
               </CarouselItem>
-
-              <CarouselItem className="space-y-6">
+              <CarouselItem className="space-y-6" key="signup">
                 <SignUp />
               </CarouselItem>
             </CarouselContent>
