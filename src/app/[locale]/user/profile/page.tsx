b3dce093f5c709d531dafffc8094a6b1c10c1020
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-12 18:31:11 +0200

    fix profile page

--- src/app/[locale]/user/profile/page.tsx
+++ src/app/[locale]/user/profile/page.tsx
@@ -16,11 +16,12 @@ const ProfilePage = () => {
     <div className="overflow-hidden bg-sky-300">
       {user ? (
         <div className="flex min-h-dvh flex-col items-center justify-evenly">
-          <p className="text-xl">Welcome, {user.user_metadata.displayName}</p>
+          <p className="text-xl text-center">Welcome<br/>
+          {user.user_metadata.displayName}</p>
           <DefaultProfileImage />
           <section>
-            <p className="text-xl">First Name: </p>
-            <p className="text-xl">Last Name: </p>
+            <p className="text-xl">First Name: {user.user_metadata.firstName}</p>
+            <p className="text-xl">Last Name: {user.user_metadata.lastName}</p>
             <p className="text-xl">Email: {user.email}</p>
           </section>
           <section>
