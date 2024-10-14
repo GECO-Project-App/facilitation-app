568b1c11989662e9a452c7613548728d67478942
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-14 16:11:27 +0200

    Fixed the tabs bg color

--- src/app/[locale]/user/page.tsx
+++ src/app/[locale]/user/page.tsx
@@ -26,16 +26,16 @@ const AuthPage = () => {
 
   return (
     <Tabs defaultValue="login" className="mx-auto h-full max-w-[600px] overflow-hidden p-4">
-      <TabsList className="m-auto mt-10 grid h-12 w-80 grid-cols-2 rounded-full bg-sky-300">
+      <TabsList className="m-auto mt-10 grid h-12 w-80 grid-cols-2 rounded-full bg-lightBlue">
         <TabsTrigger
           value="login"
-          className="text-green-100 h-10 rounded-full border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black"
+          className="text-black h-10 rounded-full border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black"
           onClick={loginTab}>
           {t('logIn')}
         </TabsTrigger>
         <TabsTrigger
           value="signup"
-          className="text-green-100 h-10 rounded-full border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black"
+          className="text-black h-10 rounded-full border-black data-[state=active]:border-2 data-[state=active]:bg-green data-[state=active]:text-black"
           onClick={signupTab}>
           {t('signUp')}
         </TabsTrigger>
