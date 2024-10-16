a001b144c01b2ed002815b4c3bbb580779c94cf7
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-16 17:53:59 +0200

    Implemented Update Password Page

--- src/app/[locale]/(tabs)/user/update-password/page.tsx
+++ src/app/[locale]/(tabs)/user/update-password/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import DialogView from '@/components/modal/DialogView';
+import {PageLayout} from '@/components';
 import {Button} from '@/components/ui/button';
 import {Input} from '@/components/ui/input';
 import {useToast} from '@/hooks/useToast';
@@ -10,7 +10,6 @@ import {useState} from 'react';
 const UpdatePassword = () => {
   const {toast} = useToast();
   const t = useTranslations('authenticate');
-  const [showDialog, setShowDialog] = useState(false);
   const [formData, setFormData] = useState({
     email: '',
     newPassword: '',
@@ -28,13 +27,12 @@ const UpdatePassword = () => {
       return;
     }
     try {
-      const { error } = await supabase.auth.updateUser({
+      const {error} = await supabase.auth.updateUser({
         email: formData.email,
         password: formData.newPassword,
       });
       if (error) throw error;
-      setShowDialog(true);
-      setFormData({ email: '', newPassword: '', confirmNewPassword: '' });
+      setFormData({email: '', newPassword: '', confirmNewPassword: ''});
     } catch (error) {
       console.error('Error updating password:', error);
       toast({
@@ -54,53 +52,55 @@ const UpdatePassword = () => {
   };
 
   return (
-        <form onSubmit={handleSubmit} className="flex min-h-[448px] h-fit flex-col justify-between">
-            <div className="flex flex-col gap-4 items-center py-10">
-                <h1 className="text-2xl font-bold">{t('updatePassword')}</h1>
-                <p className="text-sm text-gray-500">{t('updatePasswordDescription')}</p>
-            </div>
-          <div className="space-y-4 px-4">
-            <div className="space-y-2">
-              <Input
-                id="signup-password"
-                name="password"
-                type="password"
-                placeholder={t('enterPassword')}
-                value={formData.newPassword}
-                onChange={handleChange}
-                required
-                className="h-12 rounded-full"
-              />
-            </div>
-            <div className="space-y-2">
-              <Input
-                id="signup-confirm-password"
-                name="confirmPassword"
-                type="password"
-                placeholder={t('confirmPassword')}
-                value={formData.confirmNewPassword}
-                onChange={handleChange}
-                required
-                className="h-12 rounded-full"
-              />
-            </div>
-            <div className="space-y-2">
-              <Input
-                id="signup-confirm-password"
-                name="confirmPassword"
-                type="password"
-                placeholder={t('confirmPassword')}
-                value={formData.confirmNewPassword}
-                onChange={handleChange}
-                required
-                className="h-12 rounded-full"
-              />
-            </div>
+    <PageLayout>
+      <form onSubmit={handleSubmit} className="flex min-h-[448px] h-fit flex-col justify-between">
+        <div className="flex flex-col gap-4 items-center py-10">
+          <h1 className="text-2xl font-bold">{t('updatePassword')}</h1>
+          <p className="text-sm text-gray-500">{t('updatePasswordDescription')}</p>
+        </div>
+        <div className="space-y-4 px-4">
+          <div className="space-y-2">
+            <Input
+              id="signup-password"
+              name="password"
+              type="password"
+              placeholder={t('enterPassword')}
+              value={formData.newPassword}
+              onChange={handleChange}
+              required
+              className="h-12 rounded-full"
+            />
           </div>
-          <div className="mt-14 flex justify-center pb-6">
-            <Button type="submit">{t('reset')}</Button>
+          <div className="space-y-2">
+            <Input
+              id="signup-confirm-password"
+              name="confirmPassword"
+              type="password"
+              placeholder={t('confirmPassword')}
+              value={formData.confirmNewPassword}
+              onChange={handleChange}
+              required
+              className="h-12 rounded-full"
+            />
           </div>
-        </form>
+          <div className="space-y-2">
+            <Input
+              id="signup-confirm-password"
+              name="confirmPassword"
+              type="password"
+              placeholder={t('confirmPassword')}
+              value={formData.confirmNewPassword}
+              onChange={handleChange}
+              required
+              className="h-12 rounded-full"
+            />
+          </div>
+        </div>
+        <div className="mt-14 flex justify-center pb-6">
+          <Button type="submit">{t('reset')}</Button>
+        </div>
+      </form>
+    </PageLayout>
   );
 };
 
