a6f9f2ff1021b9c5529cc615b900cca2071f4525
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-21 17:37:31 +0200

    fix: the activated route affected on color of the icon at tab bar

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,30 +1,64 @@
 'use client';
 import {Link} from '@/navigation';
 import {useUserStore} from '@/store/userStore';
-import {Home, ListTodo, LogIn, Settings} from 'lucide-react';
+import {usePathname} from 'next/navigation';
 import {FC} from 'react';
 
+import {Home, Library, ListTodo, Settings, Users} from 'lucide-react';
+
 export const TabBar: FC = () => {
+  const pathname = usePathname();
   const {user} = useUserStore();
+
+  const colorText = (path: string) => {
+    if (pathname.includes(path)) {
+      return 'text-green';
+    } else {
+      return 'text-black';
+    }
+  };
 
   return (
     <nav className="fixed bottom-0 h-[40px] border-t-2 border-black bg-yellow w-full">
       <ul className="flex flex-row items-center justify-between max-w-md mx-auto h-full px-4 sm:px-0">
         <li>
-          <Link href="/">
-            <Home size={24} />
-          </Link>
-        </li>
-        <li>
           <Link href={!user ? '/user' : '/user/profile'}>
-            <LogIn size={24} />
+            <ListTodo
+              size={24}
+              className={
+                pathname.split('/').pop() === 'user' || pathname.split('/').pop() === 'profile'
+                  ? 'text-green'
+                  : 'text-black'
+              }
+            />
           </Link>
         </li>
         <li>
-          <Settings size={24} />
+          <Library size={24} className={colorText('library')} />
         </li>
         <li>
-          <ListTodo size={24} />
+          <Link href="/">
+            <Home
+              size={24}
+              className={
+                pathname.split('/').pop() === 'en' || pathname.split('/').pop() === 'sv'
+                  ? 'text-green'
+                  : 'text-black'
+              }
+            />
+          </Link>
+        </li>
+        <li>
+          <Users
+            size={24}
+            className={pathname.split('/').pop() === 'team' ? 'text-green' : 'text-black'}
+          />
+        </li>
+        <li>
+          <Settings
+            size={24}
+            className={pathname.split('/').pop() === 'settings' ? 'text-green' : 'text-black'}
+          />
         </li>
       </ul>
     </nav>
