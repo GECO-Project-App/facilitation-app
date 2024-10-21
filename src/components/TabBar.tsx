f909d29f1ee36b40229a90bc77179abc0e28dc69
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-21 15:54:24 +0200

    fix: fixed the tap bar size and use html semantic structure instead of just div

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -8,23 +8,25 @@ export const TabBar: FC = () => {
   const {user} = useUserStore();
 
   return (
-    <nav className="fixed bottom-0 flex h-[40px] w-full items-center justify-between border-t-2 border-black bg-yellow px-14">
-      <span>
-        <Link href="/">
-          <Home size={24} />
-        </Link>
-      </span>
-      <span>
-        <Link href={!user ? '/user' : '/user/profile'}>
-          <LogIn size={24} />
-        </Link>
-      </span>
-      <span>
-        <Settings size={24} />
-      </span>
-      <span>
-        <ListTodo size={24} />
-      </span>
+    <nav className="fixed bottom-0 h-[40px] border-t-2 border-black bg-yellow w-full">
+      <ul className="flex flex-row items-center justify-between max-w-md mx-auto h-full px-4 sm:px-0">
+        <li>
+          <Link href="/">
+            <Home size={24} />
+          </Link>
+        </li>
+        <li>
+          <Link href={!user ? '/user' : '/user/profile'}>
+            <LogIn size={24} />
+          </Link>
+        </li>
+        <li>
+          <Settings size={24} />
+        </li>
+        <li>
+          <ListTodo size={24} />
+        </li>
+      </ul>
     </nav>
   );
 };
