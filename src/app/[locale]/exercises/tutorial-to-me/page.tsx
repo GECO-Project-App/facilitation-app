c32befc670b74b928461cc5398e591a50ec0f2a3
Cristian Nass <cristian.tirdad@gmail.com>
2024-10-25 11:52:10 +0200

    feat: add text area from shadcn

--- src/app/[locale]/exercises/tutorial-to-me/page.tsx
+++ src/app/[locale]/exercises/tutorial-to-me/page.tsx
@@ -3,6 +3,7 @@ import {Header, PageLayout} from '@/components';
 import {CarouselPagination} from '@/components/CarouselPagination';
 import {Button} from '@/components/ui/button/button';
 import {Carousel, CarouselApi, CarouselContent, CarouselItem} from '@/components/ui/carousel';
+import {Textarea} from '@/components/ui/textarea/textarea';
 import {Step} from '@/lib/types';
 import {ArrowRight} from 'lucide-react';
 import {useTranslations} from 'next-intl';
@@ -61,32 +62,41 @@ const TutorialToMePage = () => {
           </Button>
         )
       }>
-      <section className="flex h-full w-full flex-1 items-center justify-center">
-        <Carousel className="h-full w-full flex-1" setApi={setApi}>
-          <CarouselContent>
-            {steps.map((step, index) => (
-              <CarouselItem key={index} className="space-y-6 h-full">
-                <h1 className="text-2xl font-bold">{step.title}</h1>
-                <p className="text-lg">Type your {step.title}</p>
-                <textarea
-                  rows={10}
-                  className="h-full w-full rounded-3xl border-2 border-black px-4 shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
+      <Carousel className="h-full w-full flex-1" setApi={setApi}>
+        <CarouselContent>
+          {steps.map((step, index) => (
+            <CarouselItem
+              key={index}
+              className="space-y-6 h-[68vh] overflow-y-auto overflow-x-hidden">
+              <h1 className="text-xl font-bold">{step.description}</h1>
+              <div className="grid w-full gap-1.5 text-center justify-center">
+                <label htmlFor={`${step.title} 1`}>Your {step.title} 1</label>
+                <Textarea
+                  id={`${step.title} 1`}
+                  rows={7}
+                  className="w-[88vw] rounded-xl border-2 border-black shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
                 />
-                <p className="text-lg">Type your {step.title}</p>
-                <textarea
-                  rows={10}
-                  className="h-full w-full rounded-3xl border-2 border-black px-4 shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
+              </div>
+              <div className="grid w-full gap-1.5 text-center justify-center">
+                <label htmlFor={`${step.title} 2`}>Your {step.title} 2</label>
+                <Textarea
+                  id={`${step.title} 2`}
+                  rows={7}
+                  className="w-[88vw] rounded-xl border-2 border-black shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
                 />
-                <p className="text-lg">Type your {step.title}</p>
-                <textarea
-                  rows={10}
-                  className="h-full w-full rounded-3xl border-2 border-black px-4 shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
+              </div>
+              <div className="grid w-full gap-1.5 pb-10  text-center justify-center">
+                <label htmlFor={`${step.title} 3`}>Your {step.title} 3</label>
+                <Textarea
+                  id={`${step.title} 3`}
+                  rows={7}
+                  className="w-[88vw] rounded-xl border-2 border-black shadow-[0px_6px_0px_rgb(0,0,0)] focus:outline-none"
                 />
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-      </section>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </PageLayout>
   );
 };
