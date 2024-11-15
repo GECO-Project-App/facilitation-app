dd8f8443f66ac7fb274b5c8fda64fe96ba495de9
Cristian Nass <cristian.tirdad@gmail.com>
2024-11-15 15:34:46 +0100

    fix: fix the bug about find and filter

--- src/hooks/useDoneExercise.ts
+++ src/hooks/useDoneExercise.ts
@@ -7,19 +7,18 @@ export const useDoneTutorialExercise = () => {
   const {userProfile} = useTeamStore();
   const {exercises} = useExercisesStore();
   const {currentTeam} = useTeamStore();
-  const currentTutorialExerciseId = exercises.find(
-    (e) => e.type === 'tutorial_to_me' && e.isActive,
-  )?.exerciseId;
+  const currentTutorialExerciseId =
+    exercises?.find((e) => e.type === 'tutorial_to_me' && e.isActive)?.exerciseId ?? '';
 
-  const currentExercise = exercises.filter((e) => e.exerciseId === currentTutorialExerciseId);
+  const currentExercise = exercises?.filter((e) => e.exerciseId === currentTutorialExerciseId);
   const currentUserId = userProfile?.user_id;
 
-  const didIt = currentExercise.find((ce) => ce.replied_id === currentUserId);
+  const didIt = currentExercise?.find((ce) => ce.replied_id === currentUserId);
   if (didIt) {
     done = true;
   }
   const allMembersTeamIds = currentTeam?.team_members.map((m) => m.user_id);
-  if (allMembersTeamIds?.length === currentExercise.length) {
+  if (allMembersTeamIds?.length === currentExercise?.length) {
     isAllDone = true;
   }
   return {done, isAllDone};
