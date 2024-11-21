444765348e2ea5dd8ecb853c07be8c7d3815f5d9
Cristian Nass <cristian.tirdad@gmail.com>
2024-11-21 17:40:33 +0100

    feat: implementerd grate job dialog

--- src/app/[locale]/exercises/tutorial-to-me/(tabs)/id/[slug]/review/page.tsx
+++ src/app/[locale]/exercises/tutorial-to-me/(tabs)/id/[slug]/review/page.tsx
@@ -1,5 +1,7 @@
 'use client';
+import {RiveAnimation} from '@/components';
 import {Header} from '@/components/Header';
+import DialogView from '@/components/modal/DialogView';
 import {PageLayout} from '@/components/PageLayout';
 import ReviewCompleted from '@/components/tutorial-to-me/review/ReviewCompleted';
 import ReviewNotCompleted from '@/components/tutorial-to-me/review/ReviewNotCompleted';
@@ -7,19 +9,22 @@ import {Button} from '@/components/ui';
 import {useDoneTutorialExercise} from '@/hooks/useDoneExercise';
 import {useSSCChaptersHandler} from '@/hooks/useSSCChaptersHandler';
 import {Link} from '@/i18n/routing';
+import {useDialog} from '@/store/useDialog';
 import {ArrowLeft} from 'lucide-react';
 import {useTranslations} from 'next-intl';
 import {useRouter} from 'next/navigation';
 import {FC} from 'react';
+
 const Review: FC = () => {
   const t = useTranslations('exercises.tutorialToMe');
   const {isAllDone, theTimePassed} = useDoneTutorialExercise();
   const {allReviewsDone, removeLocalStorageItem} = useSSCChaptersHandler();
+  const {isDialogOpen, setIsDialogOpen} = useDialog();
   const router = useRouter();
 
   const handleClick = () => {
     if (allReviewsDone()) {
-      router.push('/exercises/tutorial-to-me/feedback');
+      setIsDialogOpen(true);
       removeLocalStorageItem('reviewDone');
     } else {
       router.push('/');
@@ -27,29 +32,39 @@ const Review: FC = () => {
   };
 
   return (
-    <PageLayout
-      hasPadding={false}
-      backgroundColor={`bg-purple`}
-      header={
-        <Header
-          showBackButton={false}
-          leftContent={
-            <Link href={'/'}>
-              <ArrowLeft size={42} />
-            </Link>
-          }></Header>
-      }
-      footer={
-        <Button
-          variant="blue"
-          className="mx-auto"
-          disabled={!allReviewsDone()}
-          onClick={handleClick}>
-          {isAllDone ? t('reviewComplete') : t('backToHome')}
-        </Button>
-      }>
-      {isAllDone || theTimePassed ? <ReviewCompleted /> : <ReviewNotCompleted />}
-    </PageLayout>
+    <>
+      {isDialogOpen ? (
+        <DialogView
+          destinationRoute="/exercises/tutorial-to-me/feedback"
+          message={t('greatJob')}
+          sticker={<RiveAnimation src="geckograttis.riv" width={300} height={300} />}
+        />
+      ) : (
+        <PageLayout
+          hasPadding={false}
+          backgroundColor={`bg-purple`}
+          header={
+            <Header
+              showBackButton={false}
+              leftContent={
+                <Link href={'/'}>
+                  <ArrowLeft size={42} />
+                </Link>
+              }></Header>
+          }
+          footer={
+            <Button
+              variant="blue"
+              className="mx-auto"
+              disabled={!allReviewsDone()}
+              onClick={handleClick}>
+              {isAllDone ? t('reviewComplete') : t('backToHome')}
+            </Button>
+          }>
+          {isAllDone || theTimePassed ? <ReviewCompleted /> : <ReviewNotCompleted />}
+        </PageLayout>
+      )}
+    </>
   );
 };
 
