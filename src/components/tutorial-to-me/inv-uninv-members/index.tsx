a7183ef19a19bb8612d978c6ec8d646685ff4c1c
Cristian Nass <cristian.tirdad@gmail.com>
2024-11-29 13:28:53 +0100

    fix: deleted extra lines

--- src/components/tutorial-to-me/inv-uninv-members/index.tsx
+++ src/components/tutorial-to-me/inv-uninv-members/index.tsx
@@ -33,13 +33,6 @@ const InvOrDelMember: FC<InvOrDelMemberProps> = ({toturianExerciseId}) => {
       )}
       <section className="bg-yellow w-[80%] h-24 rounded-3xl border-2 border-black p-4 mx-auto flex flex-col gap-4 h-full">
         <CurrentAvatars />
-        {/* <Image
-          src="/assets/svg/inv_del_member.svg"
-          alt="Invite or Delete Member"
-          width={220}
-          height={68}
-          className="mx-auto"
-        /> */}
         {toturianExerciseId ? (
           <div className="text-center text-lg">
             {!done ? 'There is an exercises to do' : 'You already did this exercise'}
