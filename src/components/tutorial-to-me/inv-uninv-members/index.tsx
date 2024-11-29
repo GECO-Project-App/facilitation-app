7871500fee4f3bb3599994b8afff2c954a42625e
Cristian Nass <cristian.tirdad@gmail.com>
2024-11-29 11:14:24 +0100

    feat: add current team avatars

--- src/components/tutorial-to-me/inv-uninv-members/index.tsx
+++ src/components/tutorial-to-me/inv-uninv-members/index.tsx
@@ -6,8 +6,8 @@ import {getUserTeams} from '@/lib/actions/teamActions';
 import {Team} from '@/lib/types';
 import {UserRoundMinus, UserRoundPlus} from 'lucide-react';
 import {useTranslations} from 'next-intl';
-import Image from 'next/image';
 import {FC, useEffect, useState} from 'react';
+import CurrentAvatars from '../CurrentAvatars';
 import SelectTutorialTeam from '../SelectTutorialTeam';
 interface InvOrDelMemberProps {
   toturianExerciseId?: string;
@@ -32,13 +32,14 @@ const InvOrDelMember: FC<InvOrDelMemberProps> = ({toturianExerciseId}) => {
         <SelectTutorialTeam selectedTeam={teams} disableCreateOrJoin={true} />
       )}
       <section className="bg-yellow w-[80%] h-24 rounded-3xl border-2 border-black p-4 mx-auto flex flex-col gap-4 h-full">
-        <Image
+        <CurrentAvatars />
+        {/* <Image
           src="/assets/svg/inv_del_member.svg"
           alt="Invite or Delete Member"
           width={220}
           height={68}
           className="mx-auto"
-        />
+        /> */}
         {toturianExerciseId ? (
           <div className="text-center text-lg">
             {!done ? 'There is an exercises to do' : 'You already did this exercise'}
