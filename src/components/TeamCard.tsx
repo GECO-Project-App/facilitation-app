4323effc181d85b3dbeb15ad6814818d78df8551
Alice A <alice.anglesjo@hotmail.com>
2024-12-09 19:36:58 +0100

    style: code form

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -1,21 +1,64 @@
 'use client';
+import {teamCodeSchema, TeamCodeSchema} from '@/lib/zodSchemas';
 import {useTeamStore} from '@/store/teamStore';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {Rocket} from 'lucide-react';
+import {useForm} from 'react-hook-form';
 import {RemoveMember} from './icons';
 import {TeamAvatars} from './TeamAvatars';
-import {Button} from './ui';
+import {Button, Form, FormControl, FormField, FormItem, FormMessage, Input} from './ui';
 
 export const TeamCard = () => {
   const {currentTeam} = useTeamStore();
+  const form = useForm({
+    resolver: zodResolver(teamCodeSchema),
+    defaultValues: {
+      code: '',
+    },
+  });
+  const onSubmit = async (data: TeamCodeSchema) => {
+    console.log(data);
+  };
 
   return (
     <div className="md:w-[80%] mx-auto">
       {currentTeam ? (
         <div className="bg-green rounded-3xl border-2 border-black p-4  flex flex-col gap-4 h-full">
-          <TeamAvatars />
-          <Button variant="white" size="xs" className=" justify-between w-full">
-            Test
-            <RemoveMember />
-          </Button>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <p className="text-center text-base font-semibold">Can not see your team? </p>
+                <p className="text-center text-base font-semibold">Enter team code here:</p>
+              </div>
+              <FormField
+                control={form.control}
+                name="code"
+                render={({field}) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        {...field}
+                        placeholder="Enter code"
+                        className="px-4 py-2 text-sm font-normal "
+                        autoComplete="off"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="submit"
+                disabled={form.formState.isSubmitting}
+                variant="white"
+                size="xs"
+                className="mx-auto">
+                {form.formState.isSubmitting ? '...' : 'Join team'}
+                <Rocket size={20} />
+              </Button>
+            </form>
+          </Form>
         </div>
       ) : (
         <div className="bg-yellow  rounded-3xl border-2 border-black p-4 flex flex-col gap-4 h-full">
