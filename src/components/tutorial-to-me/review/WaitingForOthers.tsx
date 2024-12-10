c9a84464cf5ae865516c7b391f17a0b7b1c13bc4
Cristian Nass <cristian.tirdad@gmail.com>
2024-12-10 15:26:43 +0100

    feat: implemented the list of members dosent do the tutorial to me task

--- src/components/tutorial-to-me/review/WaitingForOthers.tsx
+++ src/components/tutorial-to-me/review/WaitingForOthers.tsx
@@ -1,6 +1,7 @@
 import {RiveAnimation} from '@/components/RiveAnimation';
 import {useTranslations} from 'next-intl';
 import {FC} from 'react';
+import UsersNotCompletedTutorialToMeTask from '../UsersNotCompletedTutorialToMeTask';
 
 const WaitingForOthers: FC<{message?: string}> = ({message}) => {
   const t = useTranslations('exercises.tutorialToMe');
@@ -8,7 +9,11 @@ const WaitingForOthers: FC<{message?: string}> = ({message}) => {
     <section className="flex flex-col items-center justify-evenly h-full space-y-8">
       <h2 className="text-2xl font-bold">{t('waiting')}</h2>
       <RiveAnimation src="timer.riv" width={300} height={300} />
-      <p className="px-4">{t('waitingForOthers')}</p>
+      <p className="px-4">
+        {t('waitingForOthersP1')}
+        <UsersNotCompletedTutorialToMeTask />
+        {t('waitingForOthersP2')}
+      </p>
     </section>
   );
 };
