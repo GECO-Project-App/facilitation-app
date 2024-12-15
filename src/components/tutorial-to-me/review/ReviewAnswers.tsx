e5faf91819dece9e6a341ec68a24fbb240bdf272
Cristian Nass <cristian.tirdad@gmail.com>
2024-12-15 16:52:24 +0100

    feat: implemented carousel

--- src/components/tutorial-to-me/review/ReviewAnswers.tsx
+++ src/components/tutorial-to-me/review/ReviewAnswers.tsx
@@ -2,62 +2,60 @@
 import {Carousel, CarouselApi, CarouselContent, CarouselItem} from '@/components/ui/carousel';
 import {useSSCChaptersHandler} from '@/hooks/useSSCChaptersHandler';
 import {useExercisesStore} from '@/store/useExercises';
-import useEmblaCarousel from 'embla-carousel-react';
 import {useRouter} from 'next/navigation';
 import {useEffect, useState} from 'react';
 import ChapterAnswer from './ChapterAnswer';
-
 export default function ReviewAnswers({chapter}: {chapter: string}) {
   const {setThisReviewDone} = useSSCChaptersHandler();
   const router = useRouter();
   const {exercises} = useExercisesStore();
-  let answersData;
-  if (chapter === 'strength') {
-    answersData = exercises.map((e) => e.answers.strengths.split(','));
-  } else if (chapter === 'weakness') {
-    answersData = exercises.map((e) => e.answers.weaknesses.split(','));
-  } else if (chapter === 'communication') {
-    answersData = exercises.map((e) => e.answers.communications.split(','));
-  }
-
-  const [emblaRef, emblaApi] = useEmblaCarousel({axis: 'y'});
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    if (!emblaApi) return;
-    emblaApi.on('select', () => {
-      setIndex(emblaApi.selectedScrollSnap());
-    });
-  }, [emblaApi]);
-
-  const chapterDone = () => {
-    console.log('chapterDone :', chapter);
-    setThisReviewDone(chapter);
-    router.back();
-  };
-  const [api, setApi] = useState<CarouselApi>();
+  const [answersData, setAnswersData] = useState<string[][]>([]);
   const [currentStep, setCurrentStep] = useState(0);
+  const [api, setApi] = useState<CarouselApi>();
 
   useEffect(() => {
+    if (chapter === 'strength') {
+      setAnswersData(exercises.map((e) => e.answers.strengths.split(',')));
+    } else if (chapter === 'weakness') {
+      setAnswersData(exercises.map((e) => e.answers.weaknesses.split(',')));
+    } else if (chapter === 'communication') {
+      setAnswersData(exercises.map((e) => e.answers.communications.split(',')));
+    }
+
     if (!api) {
       return;
     }
+
     setCurrentStep(api.selectedScrollSnap());
+
     api.on('select', () => {
       setCurrentStep(api.selectedScrollSnap());
     });
   }, [api, setCurrentStep]);
 
+  console.log('currentStep :', currentStep);
+
+  // const chapterDone = () => {
+  //   console.log('chapterDone :', chapter);
+  //   setThisReviewDone(chapter);
+  //   router.back();
+  // };
+
+  console.log('length :', answersData);
+
   return (
-    <div ref={emblaRef} className="h-full text-black overflow-hidden w-full">
-      <Carousel className="h-full w-full flex-1" setApi={setApi}>
-        <CarouselContent>
+    <div className="h-full text-black overflow-hidden w-full">
+      <Carousel className="h-full w-full flex-1" orientation="vertical" setApi={setApi}>
+        <CarouselContent className="h-[100dvh]">
           {answersData?.map((answers, i) => (
             <CarouselItem key={i} className="space-y-6">
               <ChapterAnswer key={i} chapter={chapter} answers={answers} />
             </CarouselItem>
           ))}
         </CarouselContent>
+        {/* <Button onClick={chapterDone} variant="noShadow" size="small" className="mx-auto">
+          Done
+        </Button> */}
       </Carousel>
     </div>
   );
