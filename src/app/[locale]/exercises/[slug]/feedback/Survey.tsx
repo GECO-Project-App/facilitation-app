d4fb3d776e3f5688b0412119a24f88f2ccea8bc2
Alice A <alice.anglesjo@hotmail.com>
2024-12-18 11:34:41 +0100

    style: textarea with heade variant

--- src/app/[locale]/exercises/[slug]/feedback/Survey.tsx
+++ src/app/[locale]/exercises/[slug]/feedback/Survey.tsx
@@ -1,49 +1,97 @@
 'use client';
-import {Button} from '@/components/ui/button';
-import {useKeyboardStatus} from '@/hooks/useKeyboardStatus';
+import {Button, Header, PageLayout} from '@/components';
+import {Confetti} from '@/components/icons/confetti';
+import {TextareaWithHeader} from '@/components/ui/textarea';
+import {useToast} from '@/hooks/useToast';
 import {useRouter} from '@/i18n/routing';
-import {ArrowRight} from 'lucide-react';
+import {
+  CH_IN_SURVEY_ID,
+  CH_OUT_SURVEY_ID,
+  SSC_SURVEY_ID,
+  TEST_SURVEY_ID,
+  TUTORIAL_TO_ME_SURVEY_ID,
+} from '@/lib/surveys-id';
 import {useTranslations} from 'next-intl';
-import {useState} from 'react';
-import TextArea from './views/TextArea';
+import {useParams} from 'next/navigation';
+import {usePostHog} from 'posthog-js/react';
+import {FC, useEffect, useState} from 'react';
 
-interface SurveyProps {
-  title: string;
-  onSubmit: (value: string | null) => void;
-}
-
-function Survey({title, onSubmit}: SurveyProps) {
-  const keyboardStatus = useKeyboardStatus();
-  const [selectedValue, setSelectedValue] = useState('');
+export const Survey: FC = () => {
   const router = useRouter();
+  const t = useTranslations();
+  const posthog = usePostHog();
+  const {slug} = useParams();
+  const {toast} = useToast();
+  const [surveyID, setSurveyID] = useState('');
 
-  const handleSubmit = () => {
-    onSubmit(selectedValue);
-    setSelectedValue('');
+  const getCurrentSurvey = (slug: string) => {
+    switch (slug) {
+      case 'ssc':
+        return SSC_SURVEY_ID;
+      case 'check-in':
+        return CH_IN_SURVEY_ID;
+      case 'check-out':
+        return CH_OUT_SURVEY_ID;
+      case 'tutorial-to-me':
+        return TUTORIAL_TO_ME_SURVEY_ID;
+      default:
+        return TEST_SURVEY_ID;
+    }
   };
-  const handleBack = () => {
+  useEffect(() => {
+    posthog.getActiveMatchingSurveys((surveys) => {
+      if (surveys.length > 0) {
+        const survey = surveys.find((survey) => survey.id === getCurrentSurvey(slug as string));
+        if (survey) {
+          setSurveyID(survey.id);
+        }
+      }
+    });
+  }, [posthog, slug]);
+
+  const handleSubmit = (value: string | null) => {
+    posthog.capture('survey sent', {
+      $survey_id: surveyID, // required
+      $survey_response: value, // required
+    });
+
     router.push('/');
+    toast({
+      variant: 'transparent',
+      size: 'fullscreen',
+      duration: 2000,
+      className: 'text-black bg-pink',
+      children: (
+        <>
+          <Confetti />
+          <h3 className="text-2xl font-bold">{t('feedback.thankYouText')}</h3>
+        </>
+      ),
+    });
   };
-
-  const t = useTranslations('feedback');
 
   return (
-    <article
-      className={`flex flex-col gap-4 overflow-y-hidden ${keyboardStatus.isKeyboardOpen ? 'h-[54vh] justify-between' : 'min-h-screen justify-evenly'} `}>
-      <TextArea
-        key={title}
-        title={title}
-        selectedValue={selectedValue}
-        setSelectedValue={setSelectedValue}
-        handleBack={handleBack}
-      />
-      <section className="my-2 flex justify-center">
-        <Button variant="pink" onClick={handleSubmit} disabled={!selectedValue}>
-          {t('button')} <ArrowRight />
+    <PageLayout
+      backgroundColor="bg-yellow"
+      contentColor="bg-yellow"
+      header={
+        <Header>
+          <div className="border-2 border-black rounded-full px-4 py-2 bg-pink">
+            <h3 className="text-xl font-bold">Feedback</h3>
+          </div>
+        </Header>
+      }
+      footer={
+        <Button variant="pink" onClick={() => handleSubmit(null)}>
+          {t('feedback.button')}
         </Button>
-      </section>
-    </article>
+      }>
+      <TextareaWithHeader
+        title={t(`common.slugs.${slug}`)}
+        subtitle={t('feedback.title')}
+        placeholder={t('feedback.placeholder')}
+        className="flex-grow resize-none"
+      />
+    </PageLayout>
   );
-}
-
-export default Survey;
+};
--- src/components/ui/textarea/textarea.tsx
+++ src/components/ui/textarea/textarea.tsx
@@ -1,23 +1,79 @@
+import {cva, type VariantProps} from 'class-variance-authority';
 import * as React from 'react';
 
+import {RiveAnimation} from '@/components/RiveAnimation';
 import {cn} from '@/lib/utils';
 
-// Remove the TextareaProps interface and use the supertype directly
-const Textarea = React.forwardRef<
-  HTMLTextAreaElement,
-  React.TextareaHTMLAttributes<HTMLTextAreaElement>
->(({className, ...props}, ref) => {
-  return (
-    <textarea
-      className={cn(
-        'flex min-h-[60px] w-full rounded-3xl border border-black bg-transparent p-6 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+const textAreaVariants = cva(
+  'bg-white flex min-h-[60px] max-h-[680px] w-full rounded-4xl border-2 border-black p-6  placeholder:text-black/60 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        default: '',
+        shadow: 'shadow-dark mb-3',
+      },
+    },
+    defaultVariants: {
+      variant: 'shadow',
+    },
+  },
+);
+
+interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+    VariantProps<typeof textAreaVariants> {
+  placeholder: string;
+}
+
+const hideMobileKeyboardOnReturn = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+  if (e.key === 'Enter' || e.code === 'Enter') {
+    e.preventDefault();
+    e.currentTarget.blur();
+  }
+};
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({className, variant, ...props}, ref) => {
+    return (
+      <textarea
+        inputMode="text"
+        onKeyDown={hideMobileKeyboardOnReturn}
+        className={cn(textAreaVariants({variant, className}))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
 Textarea.displayName = 'Textarea';
 
-export {Textarea};
+const TextareaWithHeader = React.forwardRef<
+  HTMLDivElement,
+  TextareaProps & {
+    title: string;
+    subtitle?: string;
+  }
+>(({className, variant, title, subtitle, ...props}, ref) => {
+  return (
+    <div className="flex flex-col flex-grow rounded-4xl ">
+      <div className="flex flex-col gap-2 bg-pink p-4 rounded-t-4xl border-2 border-black border-b-0 pt-6 relative">
+        <RiveAnimation
+          src="eyes.riv"
+          width={48}
+          height={48}
+          className=" absolute -top-6 left-0 right-0"
+        />
+        <h3 className="text-xl font-bold">{title}</h3>
+        <p className="text-black/60 ">{subtitle}</p>
+      </div>
+      <textarea
+        {...props}
+        className={cn(textAreaVariants({variant, className}), 'rounded-t-none')}
+      />
+    </div>
+  );
+});
+
+TextareaWithHeader.displayName = 'TextareaWithHeader';
+
+export {Textarea, TextareaWithHeader};
