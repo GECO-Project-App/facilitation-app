bd5d71beb2ed27e892dbe59ca964b4da4285d0e2
Alice A <alice.anglesjo@hotmail.com>
2025-01-10 14:45:17 +0100

    fix: ttm route

---
+++ src/app/[locale]/exercises/ttm/page.tsx
@@ -0,0 +1,11 @@
+import {PageLayout} from '@/components';
+
+export default function TTMExercisesPage() {
+  return (
+    <PageLayout>
+      <div>
+        <h1>TTM Exercises</h1>
+      </div>
+    </PageLayout>
+  );
+}
