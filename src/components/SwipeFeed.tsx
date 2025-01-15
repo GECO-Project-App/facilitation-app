17e0eb7e18006553b5bd5142c037bb69e1940d77
Alice A <alice.anglesjo@hotmail.com>
2025-01-15 08:10:30 +0100

    style: progressbarpage constraints for desktop

--- src/components/SwipeFeed.tsx
+++ src/components/SwipeFeed.tsx
@@ -6,19 +6,19 @@ export const SwipeFeed: FC<{backgroundColor?: string}> = ({backgroundColor = 'bg
   return (
     <div
       className={cn(
-        'h-svh w-full overflow-y-scroll snap-mandatory snap-y relative',
+        'absolute inset-0 overflow-y-scroll snap-mandatory snap-y shadow-dark rounded-4xl border-2 border-black p-6',
         backgroundColor,
       )}>
-      <h2 className="absolute top-4 left-1/2 -translate-x-1/2 text-2xl font-bold">Strengths</h2>
-      <div className="w-full h-svh snap-start snap-always flex flex-col items-center justify-center">
+      <h2 className="absolute top-16 left-1/2 -translate-x-1/2 text-2xl font-bold">Strengths</h2>
+      <div className="w-full h-full snap-start snap-always flex flex-col items-center justify-center">
         <RiveAnimation src="swipe_up.riv" height={160} width={160} />
         <h3>to read the cards</h3>
       </div>
       {Array.from({length: 10}).map((_, index) => (
         <div
           key={index}
-          className="w-full h-svh snap-start snap-always flex items-center justify-center">
-          <div className="gap-24 flex flex-col w-full">
+          className="w-full h-full snap-start snap-always flex items-center justify-center">
+          <div className="gap-8 flex flex-col w-full">
             <h3 className="text-2xl font-bold">Name</h3>
             <h3 className="text-2xl">Hello {index}</h3>
           </div>
